4b36ae4b7ebabcdb9e986c678aa47156397550bb
pauln15 <nayanpaul1530@gmail.com>
2022-12-10 17:31:35 -0600

    reservation cancellation 48 hours prior

--- frontend/src/Reservations.js
+++ frontend/src/Reservations.js
@@ -1,16 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Badge from "react-bootstrap/Badge";
 import ListGroup from "react-bootstrap/ListGroup";
 
 function Reservations(props) {
+    const [show, setShow] = useState(false);
     const [reservations, setReservations] = useState(null);
     useEffect(() => {
         componentDidMount();
-    }, [])
+    }, []);
     const componentDidMount = () => {
-        axios.get('http://localhost:3000/reservations?userid=' + props.userid)
-            .then(response => {
+        axios
+            .get("http://localhost:3000/reservations?userid=" + props.userid)
+            .then((response) => {
                 const sortedData = response.data.sort((a, b) => {
                     if (new Date(a.start_date < b.start_date)) {
                         return 1;
@@ -20,33 +22,65 @@ function Reservations(props) {
                     }
                     return 0;
                 });
-                setReservations(sortedData)
-            }).catch(error => {
-                console.log(error);
+                setReservations(sortedData);
             })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+    const deleteReservation = (reservation_id) => {
+        axios
+            .delete("http://localhost:3000/reservations/" + reservation_id)
+            .then((response) => {
+                console.log(response)
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
     return (
         <div>
             <ListGroup as="ol">
                 {reservations?.map((reservation) => {
-                    const item = props.bnbproperties.find(property => property.property_id === reservation.property_id);
-                    return (<ListGroup.Item
-                        as="li"
-                        className="d-flex justify-content-between align-items-start"
-                    >
-                        <div className="ms-2 me-auto">
-                            <div className="fw-bold">{item.property_name}</div>
-                            {item.location}
-                            <div className="fst-italic">{new Date(reservation?.start_date).toDateString().toLocaleString('en-US')} - {new Date(reservation?.end_date).toDateString().toLocaleString('en-US')}</div>
-                        </div>
-                        <Badge bg="danger" pill>
-                            Cancel Reservation
-                        </Badge>
-                    </ListGroup.Item>)
+                    const item = props.bnbproperties.find(
+                        (property) => property.property_id === reservation.property_id
+                    );
+                    const startDate = new Date(reservation?.start_date);
+                    return (
+                        <ListGroup.Item
+                            as="li"
+                            className="d-flex justify-content-between align-items-start"
+                        >
+                            <div className="ms-2 me-auto">
+                                <div className="fw-bold">{item.property_name}</div>
+                                {item.location}
+                                <div className="fst-italic">
+                                    {new Date(reservation?.start_date)
+                                        .toDateString()
+                                        .toLocaleString("en-US")}{" "}
+                                    -{" "}
+                                    {new Date(reservation?.end_date)
+                                        .toDateString()
+                                        .toLocaleString("en-US")}
+                                </div>
+                            </div>
+                            {new Date().getTime() <=
+                                new Date(
+                                    startDate.setHours(startDate.getHours() - 48)
+                                ).getTime() ? (
+                                <Badge bg="danger" pill style={{ cursor: "pointer" }} onClick={() => {
+                                    setReservations((current) => current.filter((res => res.reservation_id != reservation.reservation_id)))
+                                    deleteReservation(reservation.reservation_id)
+                                }}>
+                                    Cancel Reservation
+                                </Badge>
+                            ) : null}
+                        </ListGroup.Item>
+                    );
                 })}
             </ListGroup>
         </div>
-
     );
 }
+
 export default Reservations;
